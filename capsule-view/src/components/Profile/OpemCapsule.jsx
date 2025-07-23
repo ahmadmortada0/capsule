01c43c7c50af5fb559b58fc038a3e0b290268824
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-07-23 18:30:03 +0300

    creating Profile  components

---
+++ capsule-view/src/components/Profile/OpemCapsule.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+const OpenCapsule = ({ data }) => {
+  const [remainingTime, setRemainingTime] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const revealDate = new Date(data.revealdate);
+      
+      const diff = revealDate - now;
+      if(diff===0){
+        console.log("hello")
+        surprise(data.id)
+      }
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+
+      const formatted = `${days}d ${hours}h ${minutes}m ${seconds}s left`;
+      setRemainingTime(formatted);
+    }, 1000);
+
+    return () => clearInterval(interval);
+    
+  }, [data.revealdate]);
+  const surprise=async(id)=>{
+
+  }
+  const [date, time] = data.created_at.split("T");
+
+  return (
+    <>
+      <div className="capsule-card">
+        <div className="capsule-info">
+          <p><span>Date:</span> {date}</p>
+          <p><span>Time:</span> {time?.split(".")[0]}</p>
+          <p className="capsule-timer">{remainingTime}</p>
+        </div>
+        <button className="view-button">PENDING</button>
+      </div>
+    </>
+  );
+};
+
+export default OpenCapsule;
---
+++ capsule-view/src/components/Profile/OpenCapsules.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Surprise from './Surprise';
+
+const OpenCapsules = ({data}) => {
+  const [capsule, setCapsule] = useState(null);
+  const [ShowCapsule,setShowCapsule]=useState(false);
+  
+ const [date, time] = data.created_at.split("T");
+    const viewCapsule=()=>{
+
+    setShowCapsule(true)
+
+  }
+
+  const HideCapsule=()=>{
+
+    setShowCapsule(false)
+
+  }
+  return (
+    <>
+    {ShowCapsule&&<Surprise onclose={HideCapsule} id={data.id}/>}
+    <div className='capsule-card'>
+      <div className='capsule-info'>
+        <p className='capsule-name'>{data.name || "Capsule"}</p>
+        <p><span>Date:</span> {date}</p>
+        <p><span>Time:</span> {time?.split('.')[0]}</p>
+      </div>
+      <button className='view-button' onClick={viewCapsule}>Open</button>
+    </div>
+    </>
+  )
+}
+
+export default OpenCapsules
