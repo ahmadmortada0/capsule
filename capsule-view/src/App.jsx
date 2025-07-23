fe0ff3b538dfe5b65cd1cecf1a3bec2f4fed656b
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-07-23 19:58:34 +0300

    fixing bugs

--- capsule-view/src/App.jsx
+++ capsule-view/src/App.jsx
@@ -1,7 +1,4 @@
 import "./styles/App.css";
-import Login from "./pages/Login/Login";
-import Home from "./pages/Home/Home";
-import SignUp from "./pages/SignUp/SignUp";
 import react from "react";
 import {
   BrowserRouter,
--- capsule-view/src/routes/routes.jsx
+++ capsule-view/src/routes/routes.jsx
@@ -1,8 +1,8 @@
 import Login from "../pages/Login/Login";
 import Home from "../pages/Home/Home";
-import SignUp from "../pages/SignUp/SignUp";
 import Profile from "../pages/Profile/Profile";
 import Public from "../pages/Public/Public";
+import SignUp from "../pages/SignUp/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
@@ -13,6 +13,7 @@ const MyRoutes = () => {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/home" element={<Home />} />
       <Route path="/public" element={<Public />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
