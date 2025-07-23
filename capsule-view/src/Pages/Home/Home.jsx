20ac5f138339865f16155a6b93ad617629d09362
Ahmed Mourtada <moudi_2358@hotmail.com>
2025-07-23 18:23:45 +0300

    creating home page

---
+++ capsule-view/src/Pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../../components/Home/Header'
+import'./Home.css'
+import Body from '../../components/Home/Body';
+const Home = () => {
+
+  return (
+    
+    <div >
+    
+    <Header/>
+    
+    <Body/>
+    
+    </div>
+  )
+}
+
+export default Home
